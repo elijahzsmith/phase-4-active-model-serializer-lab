f888a347070fc90ce90e7e9a29f066e640d765dd
Elijah Smith <elijahsmith.m@gmail.com>
2022-05-18 17:45:42 -0500

    commit tree 483fb8f5719fe442970463c0e24ae3766aec9546

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author
   has_many :post_tags
   has_many :tags, through: :post_tags
+
+  private
+
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end
 end
